15c0468ee7ebe7cc37232bb51a40d8186cceb029
234989 <tregor2019@gmail.com>
2026-05-13 14:43:26 +0300

    добавил страницы, пока что пустые

--- ReadWriteNoRush/MainWindow.xaml.cs
+++ ReadWriteNoRush/MainWindow.xaml.cs
@@ -1,28 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ReadWriteNoRush.Helpers;
+using ReadWriteNoRush.Views;
 
 namespace ReadWriteNoRush
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            ConfigureSidebar();
+            MainFrame.Navigate(new CatalogPage());
+        }
+
+        private void ConfigureSidebar()
+        {
+            BtnAuthor.Visibility = AppSession.IsAuthor ? Visibility.Visible : Visibility.Collapsed;
+            BtnAdmin.Visibility = AppSession.IsAdmin ? Visibility.Visible : Visibility.Collapsed;
+            BtnFrozen.Visibility = AppSession.IsFrozen ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void BtnCatalog_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new CatalogPage());
+
+        private void BtnLists_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new ReadingListPage());
+
+        private void BtnProfile_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new ProfilePage());
+
+        private void BtnAuthor_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new AuthorPage());
+
+        private void BtnAdmin_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new AdminPage());
+
+        private void BtnFrozen_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new FrozenPage());
+
+        private void BtnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            AppSession.LogOut();
+            new AuthWindow().Show();
+            this.Close();
         }
     }
 }
